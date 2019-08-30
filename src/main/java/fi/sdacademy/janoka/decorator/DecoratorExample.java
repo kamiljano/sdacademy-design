f64820ac8bd0d70d24d65a27340f748a94190f7b
Kamil Janowski <kgjanowski@gmail.com>
2019-08-30 20:59:46 +0300

    decorator pattern

---
+++ src/main/java/fi/sdacademy/janoka/decorator/DecoratorExample.java
@@ -0,0 +1,37 @@
+package fi.sdacademy.janoka.decorator;
+
+public class DecoratorExample {
+
+    public interface ChristmasTree {
+        String decorate();
+    }
+
+    public abstract class TreeDecorator implements ChristmasTree {
+        private ChristmasTree tree;
+
+        public TreeDecorator(ChristmasTree tree) {
+            this.tree = tree;
+        }
+
+        // standard constructors
+        @Override
+        public String decorate() {
+            return tree.decorate();
+        }
+    }
+
+    public class BubbleLights extends TreeDecorator {
+
+        public BubbleLights(ChristmasTree tree) {
+            super(tree);
+        }
+
+        public String decorate() {
+            return super.decorate() + decorateWithBubbleLights();
+        }
+
+        private String decorateWithBubbleLights() {
+            return " with Bubble Lights";
+        }
+    }
+}
