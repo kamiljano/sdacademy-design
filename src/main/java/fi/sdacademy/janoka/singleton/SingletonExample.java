3b2117352ebbf3f2a2727ae962622a60bcbef901
Kamil Janowski <kgjanowski@gmail.com>
2019-08-30 20:01:33 +0300

    singleton example

---
+++ src/main/java/fi/sdacademy/janoka/singleton/SingletonExample.java
@@ -0,0 +1,14 @@
+package fi.sdacademy.janoka.singleton;
+
+public class SingletonExample {
+
+    private static SingletonExample INSTANCE;
+
+    public static SingletonExample getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SingletonExample();
+        }
+
+        return INSTANCE;
+    }
+}
