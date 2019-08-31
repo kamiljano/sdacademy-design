ccf7d2cbc96607ba5cf3ea52a1cd09076eb14173
Kamil Janowski <kgjanowski@gmail.com>
2019-08-31 19:26:01 +0300

    added lombok dependency

--- src/main/java/fi/sdacademy/janoka/patterns/singleton/SingletonExample.java
+++ src/main/java/fi/sdacademy/janoka/patterns/singleton/SingletonExample.java
@@ -12,5 +12,10 @@ public class SingletonExample {
         return INSTANCE;
     }
 
+    /**
+     * Note the private constructor. This way you can ensure that the client will always
+     * acquire the instance through the getInstance() method, since he cannot call
+     * `new SingletonExample()` even if he wanted to
+     */
     private SingletonExample() {}
 }
